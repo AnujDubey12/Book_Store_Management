79998e041969e5f05a2e96c9516051e8c2c2127c
AnujDubey12 <167118517+AnujDubey12@users.noreply.github.com>
2024-04-15 23:06:15 +0530

    Add files via upload

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Book_Store1.Controllers
+{
+    public class AccountController : Controller
+    {
+        public async Task<IActionResult> Logout()
+        {
+            // Sign out the user.
+           // await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+
+            // Redirect to home page or any other page after logout.
+            return RedirectToAction("Login", "Login");
+        }
+    }
+}
---
+++ Controllers/LoginController.cs
@@ -0,0 +1,34 @@
+﻿using Book_Store1.Models.Domain;
+using Microsoft.AspNetCore.Mvc;
+using static Book_Store1.Models.Domain.Login;
+
+namespace Book_Store1.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Login(Login model)
+        {
+            if (ModelState.IsValid)
+            {
+                // Hardcoded username and password for demonstration
+                if (model.UserName == "Anuj" && model.Password == "Anuj123")
+                {
+                    // Redirect to some other page upon successful login
+                    return RedirectToAction("GetAll", "Book");
+                }
+                else
+                {
+                    ModelState.AddModelError("", "Invalid username or password.");
+                }
+            }
+
+            // If we reach here, something went wrong, redisplay form with errors
+            return View(model);
+        }
+    }
+}
